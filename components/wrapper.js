d8328001c604c17cb1bff9bb7e8498de1f2de2f5
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2022-08-17 11:42:05 +0530

    styling added to wrapper

--- components/wrapper.js
+++ components/wrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Wrapper = ({ children }) => {
   return (
-    <section className='p-10 bg-[#101625] text-gray-50 pt-20'>
+    <section className='p-10 bg-[#101625] text-gray-50 pt-20 min-h-[100vh] scroll-smooth snap-mandatory snap-y'>
       {children}
     </section>
   );
