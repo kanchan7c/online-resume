0a6dcbc89a6b3bcf66add33fc59d9fff737aab1f
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2022-08-17 08:32:21 +0530

    nav created and responsive behaviour added

--- components/navbar.js
+++ components/navbar.js
@@ -1,24 +1,63 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import WorkIcon from '@mui/icons-material/Work';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  function showMenu() {
+    setShow(true);
+  }
+
+  function hideMenu() {
+    setShow(false);
+  }
+
   const navLinks = [
-    { name: 'Home', link: '/' },
-    { name: 'About', link: '/about' },
-    { name: 'Projects', link: '/projects' },
-    { name: 'Skills', link: '/skills' },
-    { name: 'Experience', link: '/experience' },
-    { name: 'Contact', link: '/contact' },
+    { name: 'Home', link: '/', icon: <HomeIcon /> },
+    { name: 'About', link: '/about', icon: <InfoIcon /> },
+    { name: 'Projects', link: '/projects', icon: <BusinessCenterIcon /> },
+    { name: 'Skills', link: '/skills', icon: <ListAltIcon /> },
+    { name: 'Experience', link: '/experience', icon: <WorkIcon /> },
+    { name: 'Contact', link: '/contact', icon: <ContactsIcon /> },
   ];
   return (
     <nav>
-      <ul>
+      <ul className='hidden sm:block'>
         {navLinks.map((item) => (
-          <Link href={item.link} key={item.name}>
-            {item.name}
+          <Link href={item?.link} key={item?.name}>
+            <a className='mr-2 md:mr-5  hover:text-teal-200 font-light md:text-lg'>
+              {item?.name}
+            </a>
           </Link>
         ))}
       </ul>
+      <MenuIcon
+        className={show ? 'hidden' : 'block sm:hidden'}
+        onClick={showMenu}
+      />
+      <div className='flex flex-col justify-center'>
+        <HighlightOffIcon
+          className={show ? 'block sm:hidden mb-4 ' : 'hidden'}
+          onClick={hideMenu}
+        />
+        <ul className={show ? 'block flex flex-col justify-between' : 'hidden'}>
+          {navLinks.map((item) => (
+            <Link href={item?.link} key={item?.name}>
+              <a className='mb-4 md:mr-5  hover:text-teal-200 font-light md:text-lg'>
+                {item?.icon}
+              </a>
+            </Link>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
