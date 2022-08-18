683b13af693291f3dcca1be3fc53cc62151c307d
Kanchan Chaudhary <kanchan.chaudhary77@gmail.com>
2022-08-18 20:58:34 +0530

    fixed navbar menu

--- components/navbar.js
+++ components/navbar.js
@@ -43,7 +43,10 @@ const Navbar = () => {
         ))}
       </ul>
       {show ? (
-        <HighlightOffIcon className='md:hidden lg:hidden' onClick={hideMenu} />
+        <HighlightOffIcon
+          className='md:hidden lg:hidden md:text-gray-900'
+          onClick={hideMenu}
+        />
       ) : (
         <MenuIcon className='md:hidden' onClick={showMenu} />
       )}
